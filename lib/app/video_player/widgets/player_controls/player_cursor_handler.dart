8d964b7869121f3812454b7560014f41937e2a99
Kylart <kylart.dev@gmail.com>
2025-06-11 00:03:52 +0200

    Upgraded dependencies

--- lib/app/video_player/widgets/player_controls/player_cursor_handler.dart
+++ lib/app/video_player/widgets/player_controls/player_cursor_handler.dart
@@ -158,10 +158,10 @@ class _PlayerCursorHandlerState extends State<PlayerCursorHandler> {
             });
           }
 
-          final currentBrightness = await screenBrightness.current;
+          final currentBrightness = await screenBrightness.application;
           final newBrightness = ((currentBrightness * 100 - delta) / 100.0);
 
-          screenBrightness.setScreenBrightness(
+          screenBrightness.setApplicationScreenBrightness(
             max(min(newBrightness, 1), 0),
           );
         } else {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,6 +18,7 @@ import screen_retriever_macos
 import share_plus
 import sqflite_darwin
 import url_launcher_macos
+import volume_controller
 import wakelock_plus
 import window_manager
 
@@ -35,6 +36,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
