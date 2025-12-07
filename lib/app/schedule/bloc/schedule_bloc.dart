0a50f589be6e7cc79f8a88da04644470d70f5b16
Kylart <kylart.dev@gmail.com>
2025-12-07 16:27:21 +0900

    Implemented AutoRefreshBloc to ScheduleBloc

--- lib/app/schedule/bloc/schedule_bloc.dart
+++ lib/app/schedule/bloc/schedule_bloc.dart
@@ -9,53 +9,40 @@ import 'package:anikki/domain/domain.dart';
 part 'schedule_event.dart';
 part 'schedule_state.dart';
 
-class ScheduleBloc extends Bloc<ScheduleEvent, ScheduleState> {
+class ScheduleBloc extends AutoRefreshBloc<ScheduleEvent, ScheduleState> {
   final FeedRepository feedRepository;
 
   ScheduleBloc(this.feedRepository)
-      : super(
-          ScheduleInitial(
-            feedRepository.computeRange(),
-          ),
-        ) {
+    : super(ScheduleInitial(feedRepository.computeRange())) {
     on<ScheduleRefreshed>(_onRefresh);
+
+    setUpAutoRefresh();
+  }
+
+  @override
+  Duration get intervalTime => const Duration(hours: 6);
+
+  @override
+  void autoRefresh() {
+    add(ScheduleRefreshed(range: feedRepository.computeRange()));
   }
 
   Future<void> _onRefresh(
     ScheduleRefreshed event,
     Emitter<ScheduleState> emit,
   ) async {
-    emit(
-      ScheduleLoading(
-        event.range,
-      ),
-    );
+    emit(ScheduleLoading(event.range));
 
     try {
       final entries = await feedRepository.getSchedule(range: event.range);
 
-      emit(
-        ScheduleLoaded(
-          range: event.range,
-          entries: entries,
-        ),
-      );
+      emit(ScheduleLoaded(range: event.range, entries: entries));
     } on AnilistGetScheduleException catch (e) {
-      emit(
-        ScheduleError(
-          message: e.error ?? e.cause,
-          range: event.range,
-        ),
-      );
+      emit(ScheduleError(message: e.error ?? e.cause, range: event.range));
     } catch (e) {
       logger.error('Error fetching schedule', e);
 
-      emit(
-        ScheduleError(
-          message: e.toString(),
-          range: event.range,
-        ),
-      );
+      emit(ScheduleError(message: e.toString(), range: event.range));
     }
   }
 }
